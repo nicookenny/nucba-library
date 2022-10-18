f3404dd3976db26a339ee6e97e8f965867098ff2
Kenny <kenny@nucba.com.ar>
2022-10-18 20:32:44 -0300

    f

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from 'express';
+import { UserRoles } from '../entities/Users/roles.enum';
+import { UsersService } from '../services/users.service';
 
 export class UsersController {
   constructor() {}
 
   public static async create(req: Request, res: Response) {
-    res.status(200).send({ ok: true });
+    const { email, profile } = req.body;
+    const { firstname, lastname, dni, phone, address } = profile;
+    const { street, number, zipCode, floor, apartment, city } = address;
+
+    const created = await UsersService.create({
+      email,
+      profile: {
+        firstname,
+        lastname,
+        dni,
+        phone,
+        address: {
+          street,
+          number,
+          zipCode,
+          floor,
+          apartment,
+          city,
+        },
+      },
+    });
+
+    res.status(created.success ? 200 : 400).send(created);
   }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,7 +1,59 @@
+import { prisma } from '..';
+import { UserRoles } from '../entities/Users/roles.enum';
 export class UsersService {
   constructor() {}
 
-  public static async create() {
-    return { ok: true };
+  public static async create({ email, profile }: any) {
+    try {
+      const { firstname, lastname, dni, phone, address } = profile;
+      const { street, number, zipCode, floor, apartment, city } = address;
+
+      const created = await prisma.user.create({
+        data: {
+          balance: 0,
+          role: UserRoles.CLIENT,
+          email,
+          profile: {
+            create: {
+              firstname,
+              lastname,
+              dni,
+              phone,
+              address: {
+                create: { street, number, zipCode, floor, apartment, city },
+              },
+            },
+          },
+        },
+        select: {
+          id: true,
+          email: true,
+          balance: true,
+          profile: {
+            select: {
+              firstname: true,
+              lastname: true,
+              dni: true,
+              phone: true,
+              address: {
+                select: {
+                  street: true,
+                  number: true,
+                  city: true,
+                  zipCode: true,
+                  floor: true,
+                  apartment: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return { success: true, user: created };
+    } catch (error) {
+      console.log({ error });
+      return { sucess: false, error: 'Hubo un error' };
+    }
   }
 }
